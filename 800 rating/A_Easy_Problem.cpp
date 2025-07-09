8863c632f5250d27bd2632bfb037abf7c87c9467
MD-Arpon <145496382+MD-Arpon@users.noreply.github.com>
2025-07-09 23:05:44 +0600

    Add files via upload

---
+++ 800 rating/A_Easy_Problem.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    int cnt = 0;
+    for(int i = 1,j=n-1;i<n,j>0;i++,j--)
+    {
+        cnt++;
+    }
+    cout<<cnt<<endl;
+}
+int main ()
+{
+
+    int t; cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
