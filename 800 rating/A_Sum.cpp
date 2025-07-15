5f94994c6c74133209b9b7deb938bb82bade5cb3
MD-Arpon <145496382+MD-Arpon@users.noreply.github.com>
2025-07-15 23:02:37 +0600

    Add files via upload

---
+++ 800 rating/A_Sum.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if ((a + b == c) || (b + c == a) || (a+c==b)) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
