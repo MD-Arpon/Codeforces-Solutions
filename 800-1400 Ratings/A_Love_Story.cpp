570d5f4b1b0ba706d8d127411d42a260b78e7e1a
MD-Arpon <145496382+MD-Arpon@users.noreply.github.com>
2025-08-02 21:46:57 +0600

    Add files via upload

---
+++ 800-1400 Ratings/A_Love_Story.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin>>s;
+    int cnt = 0;
+    string ss = "codeforces";
+    for(int i = 0;i<s.size();i++)
+    {
+        if(s[i]!=ss[i]) cnt++;
+    }
+    cout<<cnt<<endl;
+
+    
+}
+int main ()
+{
+
+    int t; cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
