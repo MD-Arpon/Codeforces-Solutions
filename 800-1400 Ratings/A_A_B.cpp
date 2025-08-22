50155cee6e79e7329c5c4a6611f556f5c8d26557
MD-Arpon <145496382+MD-Arpon@users.noreply.github.com>
2025-08-22 23:25:34 +0600

    Add files via upload

---
+++ 800-1400 Ratings/A_A_B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int a,b;
+    char s;
+    cin>>a>>s>>b;
+    cout<<a+b<<endl;
+}
+int main ()
+{
+
+    int t; cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
