3636031f56bac6c877cb8c33155c4c075ba0fa44
MD-Arpon <145496382+MD-Arpon@users.noreply.github.com>
2025-08-29 22:56:33 +0600

    Add files via upload

---
+++ 800-1400 Ratings/A_Young_Physicist.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+
+    int t; cin>>t;
+    int sumx = 0;
+    int sumy = 0;
+    int sumz = 0;
+    while(t--)
+    {
+        int x,y,z;
+        cin>>x>>x>>z;
+        sumx+=x,sumy+=y,sumz+=z;
+    }
+    if(sumx==0 && sumy==0 && sumz ==0) cout<<"YES";
+    else cout<<"NO";
+    return 0;
+}
